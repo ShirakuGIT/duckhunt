d19b73a1af79738b34ebd7cd1aa5780b680ee6e3
Shivaram Kumar <47910889+ShirakuGIT@users.noreply.github.com>
2020-12-07 23:05:32 +0530

    Finished crosshair updates
    Finally finished the crosshair however I have to work out the fact that the image displayed is not really transparent. I made the mouse cursor disappear but also I got the mouse cursor's x and y co-ordinates which technically I could use to get the ducks in the game go boom through some logic.

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 import pygame, sys
 from pygame.locals import *
 
-
-
 #Initializing the game window
 pygame.display.init()
 
 #This just helps with the uninitialization of the game window
 running = True
+
+gray = ( 238, 238, 238)
 
 #This initializes the framerate
 frames = 30
@@ -23,22 +23,31 @@ image = pygame.transform.scale(image, (810, 660))
 duckpic = pygame.image.load(r"duckhunt/bird.gif")
 duckpic = pygame.transform.scale(duckpic, (75, 75))
 duckpic = pygame.transform.flip(duckpic, True, False)
-duckpic = [duckpic]
+
+#Crosshair images
+c_hair = pygame.image.load(r"duckhunt/crosshair.png")
+c_hair = pygame.transform.scale(c_hair, (25, 27))
+'''c_hair = pygame.Surface.set_colorkey(None) <=== Work on this line'''
+
+pygame.mouse.set_visible(False)
 
 #This captions the game window
 pygame.display.set_caption("Duck Hunt")
-
+ 
 #display the output screen
 while running:
     display_surface.blit(image, (0, 0))
     display_surface.blit(duckpic, (200, 200))
-
-    for i in range(10):
-        display_surface.blit(duckpick[i], (i*10, 0))
+    mouse_cursor = pygame.mouse.get_pos()
+    x = mouse_cursor[0]
+    y = mouse_cursor[1]
+    display_surface.blit(c_hair, [x, y])
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
 
     pygame.display.update()
     pygame.time.Clock().tick(frames)
